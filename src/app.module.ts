f211c0bdbd34e8751fddd8ceddfff3f32101ce94
Thaís <tscampos31@gmail.com>
2025-12-15 15:56:16 -0300

    atualização do deploy - blog pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
+import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
@@ -12,9 +15,20 @@ import { ProdService } from './data/services/prod.service';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService, 
-      imports: [ConfigModule],
+      useClass: ProdService,
+      imports: [ConfigModule]
     }),
+    //Testar local para fazer teste no insomnia
+   /*TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'db_blog_pessoal',
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true,
+    }), */
     PostagemModule,
     TemaModule,
     AuthModule,
